3752d34553f051a435fdea5995cd5c384ed377cd
Your Name <you@example.com>
2023-03-31 10:50:07 +0530

    Loggin Page Completed

--- Pages/Loggin.js
+++ Pages/Loggin.js
@@ -1,41 +1,63 @@
-import { View, Text, ImageBackground, StyleSheet } from 'react-native'
+import { View, Text, ImageBackground, StyleSheet, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { TextInput } from 'react-native-paper';
+
 
 
 export default function Loggin(navigation) {
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <ImageBackground style={styles.backgroundImg3} source={require('../assets/img3.png')}></ImageBackground>
+        <ImageBackground style={styles.backgroundImg} source={require('../assets/img3.png')}></ImageBackground>
       </View>
 
       <Text style={styles.baseText}>Log in</Text>
 
       <View style={styles.container2}>
 
-      <Text style={{marginLeft:'5%', marginBottom:'-0%',marginTop:'-4%', color:'#D1D0D1',fontSize:18,}}>Email</Text>
+        <Text style={{ marginLeft: '5%', marginBottom: '0%', marginTop: '1%', color: '#D1D0D1', fontSize: 18, }}>Email</Text>
 
         <View style={styles.input1}>
           <TextInput
-          style={{marginBottom:'4%',borderRadius:'20%'}}
+            style={{ borderColor: '#D1D0D1' }}
+            placeholderTextColor={'#AAA7A9'}
             placeholder="acpt@milithdesilva.com"
             underlineColorAndroid="transparent"
-            backgroundColor="#000000"  
+            backgroundColor="#FFFFFF"
+
+
           />
         </View>
 
-       <Text style={{marginLeft:'5%', marginBottom:'0%',marginTop:'5%', color:'#D1D0D1',fontSize:16,}}>Password</Text> 
+        <Text style={{ marginLeft: '5%', marginBottom: '0%', marginTop: '5%', color: '#D1D0D1', fontSize: 16, }}>Password</Text>
 
         <View style={styles.input2}>
           <TextInput
-             label=""
+            style={{ borderColor: '#D1D0D1' }}
             onChangeText={text => setText(text)}
             underlineColorAndroid="transparent"
             backgroundColor="#FFFFFF"
+
           />
         </View>
+
+        <View style={{ flex: 1, justifyContent: 'center', marginTop:'20%' }}>
+          <TouchableOpacity style={styles.btn} >
+            <Text style={{ fontSize: 20, color: '#E3E4E4', textAlign: 'center', fontWeight: 'bold' }}>Login Now</Text>
+          </TouchableOpacity>
+
+          <Text style={{ textAlign: 'center', marginTop: '3%' }}>Forget Password</Text>
+        </View>
+
+        <View style={{ flex: 1, flexDirection: 'row',marginTop:'10%', marginLeft:'39%' }}>
+          <Image style={{ width: 25, height: 25 }} source={require('../assets/facebook.png')} />
+          <Image style={{ width: 25, height: 25 }} source={require('../assets/google.png')} />
+          <Image style={{ width: 25, height: 25 }} source={require('../assets/linkedin.png')} />
+        </View>
+
+
+
       </View>
 
 
@@ -58,35 +80,35 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFFFFF',
   },
 
-  backgroundImg3: {
+  backgroundImg: {
     flex: 2,
     flexDirection: 'row',
-    height: 250,
-    width: 355,
+    height: 190,
+    width: 320,
+    justifyContent: 'center'
   },
   baseText: {
     flex: 1,
     flexDirection: 'row',
     fontSize: 40,
     fontWeight: 'bold',
-    marginTop: '65%',
+    marginTop: '50%',
     marginBottom: '-15%',
     marginLeft: '8%',
     color: '#3F3E4C'
   },
 
   container2: {
-    flex: 2,
+    flex: 3,
     flexDirection: 'column',
     width: 300,
     marginLeft: '5%',
-    
-  
+
+
   },
 
   input1: {
-    // flex: 1,
-    // flexDirection: 'column',
+
     // marginLeft: '5%',
     // width: 300,
     // marginTop: '5%',
@@ -95,8 +117,21 @@ const styles = StyleSheet.create({
   input2: {
     // marginLeft: '5%',
     // width: 300,
-    // marginTop: '10%'
+    marginTop: '-1%',
+    marginBottom: '-10%',
+
   },
+  btn: {
+    alignItems: 'center',
+    backgroundColor: '#007DFE',
+    padding: 10,
+    fontWeight: 'bold',
+    borderRadius: 30,
+    width: 200,
+    marginLeft: '18%',
+
+
+  }
 
 
   //  mail: {
