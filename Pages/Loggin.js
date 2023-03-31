d84dbf5c03406737a4d5305c02939b0dc2418cb6
Your Name <you@example.com>
2023-03-31 08:17:09 +0530

    Logging Page Update

--- Pages/Loggin.js
+++ Pages/Loggin.js
@@ -13,32 +13,31 @@ export default function Loggin(navigation) {
 
       <Text style={styles.baseText}>Log in</Text>
 
-      <View>
-        <Text style={styles.mail}>Email</Text>
+      <View style={styles.container2}>
+
+      <Text style={{marginLeft:'5%', marginBottom:'-0%',marginTop:'-4%', color:'#D1D0D1',fontSize:18,}}>Email</Text>
+
+        <View style={styles.input1}>
+          <TextInput
+          style={{marginBottom:'4%',borderRadius:'20%'}}
+            placeholder="acpt@milithdesilva.com"
+            underlineColorAndroid="transparent"
+            backgroundColor="#000000"  
+          />
+        </View>
+
+       <Text style={{marginLeft:'5%', marginBottom:'0%',marginTop:'5%', color:'#D1D0D1',fontSize:16,}}>Password</Text> 
+
+        <View style={styles.input2}>
+          <TextInput
+             label=""
+            onChangeText={text => setText(text)}
+            underlineColorAndroid="transparent"
+            backgroundColor="#FFFFFF"
+          />
+        </View>
       </View>
 
-      <View style={styles.input1}>
-        <TextInput
-          label="Enter Your Email"
-          onChangeText={text => setText(text)}
-          mode="Flat"
-          backgroundColor="#FFFFFF"
-
-        />
-      </View>
-
-      <View>
-        <Text style={styles.psw}>Password</Text>
-      </View>
-
-      <View style={styles.input2}>
-        <TextInput
-          label="Enter Your Password"
-          onChangeText={text => setText(text)}
-          mode="Outlined(disabled)"
-          backgroundColor="#FFFFFF"
-        />
-      </View>
 
 
     </SafeAreaView>
@@ -46,50 +45,77 @@ export default function Loggin(navigation) {
   )
 }
 
+
+
+
+
+
+
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#FFFFFF',
   },
 
   backgroundImg3: {
-    flex: 4,
+    flex: 2,
+    flexDirection: 'row',
     height: 250,
     width: 355,
   },
   baseText: {
-    fontSize: 50,
-    marginTop: '60%',
-    justifyContent: 'center',
+    flex: 1,
+    flexDirection: 'row',
+    fontSize: 40,
     fontWeight: 'bold',
-    marginLeft: '5%',
+    marginTop: '65%',
+    marginBottom: '-15%',
+    marginLeft: '8%',
     color: '#3F3E4C'
   },
-  input1: {
-    marginLeft: '5%',
-    width: 300,
-    marginTop: '5%',
 
+  container2: {
+    flex: 2,
+    flexDirection: 'column',
+    width: 300,
+    marginLeft: '5%',
+    
+  
+  },
+
+  input1: {
+    // flex: 1,
+    // flexDirection: 'column',
+    // marginLeft: '5%',
+    // width: 300,
+    // marginTop: '5%',
 
   },
   input2: {
-
-    marginTop: '10%'
+    // marginLeft: '5%',
+    // width: 300,
+    // marginTop: '10%'
   },
-  mail: {
-
-    marginLeft: '5%',
-    marginTop: '1%',
-    marginBottom: '1%',
-    color: '#D1D0D1'
-  },
-  psw: {
-    marginLeft: '5%',
-    marginTop: '2%',
-    marginBottom: '0sss%',
-    color: '#D1D0D1'
-  }
 
 
+  //  mail: {
+
+  //    marginLeft: '5%',
+  //    marginTop: '1%',
+  //    marginBottom: '1%',
+  //    color: '#D1D0D1'
+  //  },
+  //  psw: {
+  //    marginLeft: '5%',
+  //    marginTop: '2%',
+  //    marginBottom: '0sss%',
+  //    color: '#D1D0D1'
+  //  }
+
+  // 
+
+  // <Text style={styles.psw}>Password</Text>
 });
 
 
